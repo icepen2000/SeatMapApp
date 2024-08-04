57080ee2eb3bc8f253c6f6f2f31ca4fc9872bacb
icepen2000 <icepen2000@gmail.com>
2024-08-04 11:31:01 -0700

    Merged with Jihoon's performance enhance. It also treats selected seats differently.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
-  backendUrl: '10.0.2.2:8090', // default to localhost
-    // backendUrl: '34.238.103.127:8090',
+  //backendUrl: '10.0.2.2:8090', // default to localhost
+  backendUrl: '34.238.103.127:8090',
 };
 
 export const getBackendUrl = () => `http://${config.backendUrl}`;
